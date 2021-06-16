20c0133eb3025822a7dccedc289115e4faf20e6b
NRMAIM <85989254+NRMAIM@users.noreply.github.com>
2021-06-16 10:32:53 +0200

    Add files via upload

---
+++ h4ckme-master/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Components/Main";
+import Details from "./Components/Details";
+import Login from "./Components/Login";
+import Create from "./Components/Create";
+import Secret from "./Components/Secret";
+
+import ArticleStore from "./Stores/Article";
+import UserStore from "./Stores/User";
+import { Route, Switch, Redirect } from "react-router";
+import { observer } from "mobx-react";
+import { toJS } from "mobx";
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Main
+              {...props}
+              articles={toJS(ArticleStore.articles)}
+              loading={toJS(ArticleStore.loading)}
+            />
+          )}
+        />
+        <Route path="/detail/:articleID" component={Details} />
+        <Route path="/login" component={Login} />
+        <Route path="/create" component={Create} />
+        <Route path="/secret" component={Secret} />
+
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+}
+
+export default observer(App);
